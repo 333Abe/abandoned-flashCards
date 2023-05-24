be96aa6d6fa57f1d0532c4514263075e57fc9c59
333Abe <adrianbennett333@gmail.com>
2023-05-24 16:33:06 +0100

    create views.topics

--- flash_cards/views.py
+++ flash_cards/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Topic, SubTopic, CardQuestion, Choice, Resource
+
 def index(request):
     """flashCards home page"""
     return render(request, 'flash_cards/index.html')
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.all()
+    context = {'topics': topics}
+    return render(request, 'flash_cards/topics.html', context)
