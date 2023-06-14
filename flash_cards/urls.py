f88d03951924e642c8fb07aca24d3fedf140e51d
333Abe <adrianbennett333@gmail.com>
2023-06-14 14:44:32 +0100

    create url patterns for new_subtopic/, quiz/, results/

--- flash_cards/urls.py
+++ flash_cards/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     path('topics/<int:topic_id>/subtopics/', views.subtopics, name='subtopics'),
     # New topic page
     path('new_topic/', views.new_topic, name='new_topic'),
+    # New subtopic page
+    path('topics/<int:topic_id>/new_subtopic/', views.new_subtopic, name='new_subtopic'),
+    # Quiz page
+    path('quiz/', views.start_quiz, name='start_quiz'),
+    # Results page
+    path('results/', views.results, name='results')
 ]
