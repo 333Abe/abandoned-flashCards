2010b0576e42d796678e023ab17aa20269eeb78e
333Abe <adrianbennett333@gmail.com>
2023-06-14 14:38:32 +0100

    create new subtopic modelform

--- flash_cards/forms.py
+++ flash_cards/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, SubTopic
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
-        
+
+class SubtopicForm(forms.ModelForm):
+    class Meta:
+        model = SubTopic
+        fields = ['topic', 'text']
